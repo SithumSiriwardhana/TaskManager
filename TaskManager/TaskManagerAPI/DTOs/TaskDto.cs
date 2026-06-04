cd25cd39bc0ac5e82ddbc0cda952b8d79fba7721
Sithum Siriwardhana <sithum@enhanzer.com>
2026-06-04 15:35:22 +0530

    Create DTOs

---
+++ TaskManager/TaskManagerAPI/DTOs/TaskDto.cs
@@ -0,0 +1,35 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagementAPI.DTOs;
+
+public class CreateTaskDto
+{
+    [Required]
+    [MaxLength(200)]
+    public string Title { get; set; } = string.Empty;
+
+    [MaxLength(500)]
+    public string? Description { get; set; }
+
+    public DateTime? DueDate { get; set; }
+
+    [Range(1, 3)]
+    public int Priority { get; set; } = 1;
+}
+
+public class UpdateTaskDto
+{
+    [Required]
+    [MaxLength(200)]
+    public string Title { get; set; } = string.Empty;
+
+    [MaxLength(500)]
+    public string? Description { get; set; }
+
+    public bool IsCompleted { get; set; }
+
+    public DateTime? DueDate { get; set; }
+
+    [Range(1, 3)]
+    public int Priority { get; set; } = 1;
+}
